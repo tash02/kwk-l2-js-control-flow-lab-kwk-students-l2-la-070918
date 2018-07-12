c5f9d333639e4583e641e5b30bd3684ba1994d84
tash02 <tash.brinkley@gmail.com>
2018-07-12 19:21:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet<= 400) {
+    result = ("This one is on me!")
+    return result
+  }
+   else if (feet>= 2500) {
+      result = ("No can do.")
+      return result
+    }
+    else if (feet>2000) {
+      result = ("I will gladly take your thirty bucks.")
+      return result
+    }
+  }
+  
+  function ternaryCheckCity(city) {
+    if (city == "NYC") {
+      result = ("Ok, sounds good.")
+      return result
+    }
+    else if (city !== "NYC"){
+      result = ("No go.")
+      return result
+    }
+  }
+  
+  function switchOnCharmFromTip(tip) {
+    if (tip == "generous") {
+      result = ("Thank you so much.")
+      return result
+    }
+  }
+
+   
